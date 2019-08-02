d62b0211a99a40a9a26b11470f0cd23763ce82b4
Matheus H. de Souza <mh.matheussouza@gmail.com>
2019-08-02 15:30:50 -0300

    Post view created

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -37,4 +37,9 @@ class PostsController extends Controller
 
         return redirect('/profile/' . auth()->user()->id);
     }
+
+    public function show(Post $post)
+    {
+        return view('posts.show', compact('post'));
+    }
 }
