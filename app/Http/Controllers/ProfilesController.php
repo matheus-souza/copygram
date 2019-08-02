6f79cd0f74425e82b777ff0f8d78a6766951a2f4
Matheus H. de Souza <mh.matheussouza@gmail.com>
2019-08-02 11:31:48 -0300

    Created profile route

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
 {
-    //
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index($user)
+    {
+        $user = User::findOrFail($user);
+        return view('home', compact('user'));
+    }
 }
